9a013e1beb67ccb9167836ae07a063ebb67d4288
George Rotteny <george.lopes@safetydocs.com.br>
2024-05-19 11:28:10 -0300

    melhoria no código

--- src/BaseClass.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Rotteny\BrasilApi;
-
-class BaseClass {
-
-    const BASE_URL = 'https://brasilapi.com.br/api/cep/v1/';
-
-}
--- src/CepV2.php
+++ src/CepV2.php
@@ -2,12 +2,12 @@
 
 namespace Rotteny\BrasilApi;
 
-class CepV2 extends BaseClass {
+class CepV2 extends Cep {
     
     public static function get(string $cep) : object {
         try {
             $client = new \GuzzleHttp\Client();
-            $response = $client->get(self::BASE_URL . $cep);
+            $response = $client->get(self::BASE_URL . 'v2/' . $cep);
             return json_decode($response->getBody());
         } catch (\Throwable $th) {
             die($th->getMessage());
